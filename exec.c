70e346c8b491889afc3719862be91103055c2838
JoeKing08 <262213181@qq.com>
2025-11-22 19:37:59 +0800

    share-mem-only mode

--- exec.c
+++ exec.c
@@ -21,6 +21,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
+#include "dsm_backend.h"
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #ifndef _WIN32
