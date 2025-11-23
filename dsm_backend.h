dc26f022b1f291ed59dd63abaf7af8c8bc2b1180
JoeKing08 <262213181@qq.com>
2025-11-23 10:06:34 +0800

    share-mem-only mode

--- dsm_backend.h
+++ dsm_backend.h
@@ -1,11 +1,14 @@
-/* dsm_backend.h - GiantVM Ultimate Extension */
+/* GiantVM Ultimate Extension */
 #ifndef DSM_BACKEND_H
 #define DSM_BACKEND_H
+
 #include <stddef.h>
 #include <stdint.h>
 
-extern int dsm_mode;
-void dsm_param_init(void);
-void dsm_auto_setup(void);
-void dsm_register_ram(void *ptr, size_t size);
+/* 全局模式标志：0=原版内核模式, 1=UFFD内存模式 */
+extern int gvm_mode;
+
+void dsm_universal_init(void);
+void dsm_universal_register(void *ptr, size_t size);
+
 #endif
